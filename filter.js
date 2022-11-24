6144ec3b8531d154d1730c4ee74d0dcf5a4de66c
Z-kamili <zakariakamili97@gmail.com>
2022-11-24 13:50:26 +0000

    more code

--- filter.js
+++ filter.js
@@ -1,5 +1,7 @@
 const words = ['spray','limit','elite','exuberant','destruction','present'];
 
-const result = words.filter(word => word.length > 6);
+
+
+const result = words.filter(word => word.length > 4);
 
 console.log(result);
