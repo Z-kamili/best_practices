ca80f6fea4920be8d4ce07724b3d45c6004b0934
Z-kamili <zakariakamili97@gmail.com>
2022-11-24 12:26:17 +0000

    add code

---
+++ index.js
@@ -0,0 +1,24 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+const kvArray = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+];
+const kvArray_2 = [
+
+  { key: 1 , value: 10},
+  { key: 2 , value: 20},
+  { key: 3 , value: 30},
+
+];
+
+const reformattedArray = kvArray.map(({ key, value }) => ({ [key]: value }));
+const reformatteArray =  kvArray.map(({key,value}) => ({ [key] : value }));
+console.log(reformatteArray);
