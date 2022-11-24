a3b75ac716dcf9cbf8329d976ab6f85c6fd407fe
Z-kamili <zakariakamili97@gmail.com>
2022-11-24 15:18:48 +0000

    create a new object into map

--- examples.js
+++ examples.js
@@ -101,5 +101,26 @@ let sizes = cars.map(car => {
 console.log(sizes);
 
 
+let carsProperties = cars.map(car => {
+
+    let properties = {
+        "capacity":car.capacity,
+        "size":"large"
+    };
+
+    if(car.capacity <= 5) {
+        properties['size'] = "medium";
+    }
+
+    if(car.capacity <= 3) {
+        properties['size'] = "small";
+    }
+
+    return properties;
+
+});
+
+console.log(carsProperties);
+
 
 
