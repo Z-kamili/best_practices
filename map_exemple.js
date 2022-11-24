f7cc6a71b5a78bde857358d262b4595db45d015f
Z-kamili <zakariakamili97@gmail.com>
2022-11-24 14:17:20 +0000

    more code

---
+++ map_exemple.js
@@ -0,0 +1,12 @@
+const sweetArray = [2,3,4,5,35];
+
+const sweeterArray = sweetArray.map(sweetItem => {
+    return sweetItem * 2
+});
+
+console.log(sweetArray);
+
+
+const sweetArray_2 = sweetArray.map(sweetItem => {
+    return sweetItem *4;
+});
