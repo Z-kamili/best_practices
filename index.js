b9127442991cfc9dfcd92e80138d56c0dfcbb779
Z-kamili <zakariakamili97@gmail.com>
2022-11-24 12:29:32 +0000

    solve problem

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const message = 'Hello world' // Try edit me
+// const message = 'Hello world' // Try edit me
 
 // Update header text
-document.querySelector('#header').innerHTML = message
+// document.querySelector('#header').innerHTML = message
 
 // Log to console
-console.log(message)
+// console.log(message)
 
-const kvArray = [
-  { key: 1, value: 10 },
-  { key: 2, value: 20 },
-  { key: 3, value: 30 },
-];
+// const kvArray = [
+//   { key: 1, value: 10 },
+//   { key: 2, value: 20 },
+//   { key: 3, value: 30 },
+// ];
 const kvArray_2 = [
 
   { key: 1 , value: 10},
@@ -19,6 +19,6 @@ const kvArray_2 = [
 
 ];
 
-const reformattedArray = kvArray.map(({ key, value }) => ({ [key]: value }));
-const reformatteArray =  kvArray.map(({key,value}) => ({ [key] : value }));
+const reformattedArray = kvArray_2.map(({ key, value }) => ({ [key]: value }));
+const reformatteArray =  kvArray_2.map(({key,value}) => ({ [key] : value }));
 console.log(reformatteArray);
